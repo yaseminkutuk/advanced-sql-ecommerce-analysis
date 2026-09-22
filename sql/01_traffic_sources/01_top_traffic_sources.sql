ae134279be02e12b5d4b145817c423158fc7e86f
yasemin <142181859+yaseminkutuk@users.noreply.github.com>
2026-09-22 23:24:27 +0300

    Update 01_top_traffic_sources.sql

--- sql/01_traffic_sources/01_top_traffic_sources.sql
+++ sql/01_traffic_sources/01_top_traffic_sources.sql
@@ -4,7 +4,7 @@
 
 -- Where is the majority of website traffic coming from?
 -- Break down sessions by UTM source, campaign, and referring domain.
--- Analysis date: 2012-04-12
+-- Request date: 2012-04-12
 
 select utm_source, utm_campaign, http_referer, count(*) as sessions
 from website_sessions
