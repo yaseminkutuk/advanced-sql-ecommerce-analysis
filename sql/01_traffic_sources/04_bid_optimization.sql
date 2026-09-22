9f3934214a0b6e80ed73b6efe275fd7e78b9975d
yasemin <142181859+yaseminkutuk@users.noreply.github.com>
2026-09-22 17:57:40 +0300

    Create 04_bid_optimization.sql

---
+++ sql/01_traffic_sources/04_bid_optimization.sql
@@ -0,0 +1,17 @@
+-- 	4. Bid Optimization for Paid Traffic
+
+-- Business Question:
+-- What is the conversion rate from session to order by device type?
+-- Analysis date: 2012-05-11
+
+select 
+ws.device_type,
+count(distinct ws.website_session_id) as sessions,
+count(distinct o.order_id) as orders,
+count(distinct o.order_id)/count(distinct ws.website_session_id)  as conv_rate
+from website_sessions ws
+left join orders o on o.website_session_id = ws.website_session_id
+where ws.created_at < "2012-05-11"
+and ws.utm_source = "gsearch"
+and ws.utm_campaign = "nonbrand"
+group by ws.device_type;
