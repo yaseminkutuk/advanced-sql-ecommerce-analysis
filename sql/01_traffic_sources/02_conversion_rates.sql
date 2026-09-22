a0f661835ac0612914d73f01dcd6773b6e42a747
yasemin <142181859+yaseminkutuk@users.noreply.github.com>
2026-09-22 17:55:59 +0300

    Create 02_conversion_rates.sql

---
+++ sql/01_traffic_sources/02_conversion_rates.sql
@@ -0,0 +1,15 @@
+-- 	2. Traffic Source Cenversion Rates
+
+-- 	Business Question:
+-- What is the conversion rate from session to order for the major traffic source?
+-- Analysis date: 2012-04-14
+
+select 
+count(distinct ws.website_session_id) as sessions,
+count(distinct o.order_id) as orders,
+count(distinct o.order_id)/count(distinct ws.website_session_id)  as conv_rate
+from website_sessions ws
+left join orders o on o.website_session_id = ws.website_session_id
+where ws.created_at < "2012-04-14"
+and ws.utm_source = "gsearch"
+and ws.utm_campaign = "nonbrand";
