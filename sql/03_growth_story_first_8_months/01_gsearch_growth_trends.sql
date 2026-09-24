4b09f4e3b6da91550e18733da543a6216a97adb4
yasemin <142181859+yaseminkutuk@users.noreply.github.com>
2026-09-24 18:58:03 +0300

    Create 01_gsearch_growth_trends.sql

---
+++ sql/03_growth_story_first_8_months/01_gsearch_growth_trends.sql
@@ -0,0 +1,14 @@
+-- Business Question 1:
+-- Gsearch seems to be the biggest driver of our business. 
+-- Could you pull monthly trends for gsearch sessions and orders so that we can showcase the growth
+-- Request date: 2012-11-27
+
+select 
+month(ws.created_at) as month,
+count(ws.website_session_id) as sessions,
+count(o.order_id) as orders
+from website_sessions as ws
+left join orders as o on o.website_session_id = ws.website_session_id
+where ws.created_at < "2012-11-27"
+and ws.utm_source ="gsearch"
+group by month(ws.created_at);
